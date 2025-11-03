19233ad122a705e2a8bb5b90396809754faac80c
Abdul Sami Shaikh <abdulsamishaikh232@gmail.com>
2025-11-03 11:15:55 +0530

    day

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const PI = 3.14159;
-let radius ;
-let circumference;
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 30.31;
+let e = 30.99;
+let f = 0;
+let g = 0;
+let x = 0;
+let y =0;
+let z =0;
 
-document.getElementById("mySubmit").onclick = function(){
-  radius = document.getElementById("myText").value ;
-  radius = Number(radius);
-  circumference = 2 * PI * radius;
-  document.getElementById("myH3").textContent = circumference + " cm" ;
-}
+let max = Math.max(a, b, c);
+let min = Math.min(a, b, c);
+let k
+k = Math.ceil(d);
+let j
+j = Math.floor(e);
+let o
+o = Math.trunc(e);
+console.log (`The Max is ${max}`);
+console.log (`The Min is ${min}`);
+console.log (`The round up is ${k}`)
+console.log (`The round down is ${j}`)
+console.log(`the trunc is ${o}`);
